4f71fb59abc2bcbe437c0f83bb0fe5dfdff7543b
dwqs <461147874@qq.com>
2018-09-23 16:25:08 +0800

    去掉混淆

--- rollup.build.config.js
+++ rollup.build.config.js
@@ -3,21 +3,21 @@ import { minify } from 'uglify-es';
 
 import config from './rollup.config';
 
-config.plugins.push(
-    uglify({
-        compress: {
-            warnings: false,
-            drop_debugger: true,
-            drop_console: true
-        },
-        mangle: true
-    }, minify)
-);
+// config.plugins.push(
+//     uglify({
+//         compress: {
+//             warnings: false,
+//             drop_debugger: true,
+//             drop_console: true
+//         },
+//         mangle: true
+//     }, minify)
+// );
 
 export default {
     entry: 'src/index.ts',
     format: 'umd',
     moduleName: 'ReduxActionsPromise',
-    dest: 'dist/index.min.js',
+    dest: 'dist/index.js',
     plugins: config.plugins
 };
